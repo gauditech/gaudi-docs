661e4f71dc84bd0e3b95df42d213eae65efe4aa9
Hrvoje Kasabasic <hrvoje.kasabasic@mashpipe.io>
2023-07-30 01:50:26 +0200

    warn on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: "gaudi", // Usually your GitHub org/user name.
   projectName: "gaudi-docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
